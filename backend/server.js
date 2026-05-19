bf0e8a31a65136a3cbb1f664065ea9b687fd8739
vuhoang <ojtplf@sendo.vn>
2026-05-19 11:45:45 +0700

    repair to deploy vercel

--- backend/server.js
+++ backend/server.js
@@ -49,4 +49,8 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+if (process.env.NODE_ENV !== 'production') {
+  server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+}
+
+module.exports = app;
